e7803c04e143e9fc7050886bf7a111e2bce3c678
Alessio Pascucci <feeder81@gmail.com>
2025-02-15 20:17:23 +0100

    Add helper
    Signed-off-by: Alessio Pascucci <feeder81@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ mod sender;
 mod receiver;
 mod common;
 
+fn print_usage() {
+    println!("Usage:");
+    println!("  send <address> <file_path> - Send a file to the specified address");
+    println!("  receive - Receive files on the specified port");
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 4 && args[1] == "send" {
